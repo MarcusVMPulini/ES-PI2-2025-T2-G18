1f271fb25fda548cc0111f08806e95e9a6ba3a85
Lucas Leonel <lucleonelg4@gmail.com>
2025-11-17 18:54:36 -0300

    comentario, autor

--- backend/src/routes/boletim.routes.ts
+++ backend/src/routes/boletim.routes.ts
@@ -1,9 +1,12 @@
+//Autor: Marcus
+
+//imports
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { boletimPorAluno } from "../controllers/boletim.controller";
-
+//variavel manipulacao
 const router = Router();
-
+//rotas
 router.use(authMiddleware);
 
 router.get("/:idAluno", boletimPorAluno);
