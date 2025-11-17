e2399c28718f912f94fc15bfd0a1ca3412c2cf23
Lucas Leonel <lucleonelg4@gmail.com>
2025-11-17 18:54:06 -0300

    comentario, autor

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,8 +1,11 @@
+//Autor: Marcus
+
+//imports
 import { Router } from "express";
 import { register, login, forgotPassword, resetPassword } from "../controllers/auth.controller";
-
+//variavel manipulacao
 const router = Router();
-
+//rotas
 router.post("/register", register);
 router.post("/login", login);
 router.post("/forgot-password", forgotPassword);
